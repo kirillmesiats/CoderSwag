175ea3eba5c550191fbde5ffd8d0f96501788ad7
Kirill Mesiats <a55a4@yandex.ru>
2018-09-21 15:49:09 +0300

    create a layout for categories

--- app/src/main/java/relsys/eu/coderswag/controller/MainActivity.kt
+++ app/src/main/java/relsys/eu/coderswag/controller/MainActivity.kt
@@ -1,13 +1,24 @@
 package relsys.eu.coderswag.controller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import relsys.eu.coderswag.R
+import relsys.eu.coderswag.model.Category
+import relsys.eu.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/relsys/eu/coderswag/model/Category.kt
+++ app/src/main/java/relsys/eu/coderswag/model/Category.kt
@@ -1,3 +1,8 @@
 package relsys.eu.coderswag.model
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+
+    override fun toString(): String {
+        return title
+    }
+}
