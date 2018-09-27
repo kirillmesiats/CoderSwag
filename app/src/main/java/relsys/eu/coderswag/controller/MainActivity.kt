5e05d13e928b6392a52d24e059c160c8c301c282
Kirill Mesiats <a55a4@yandex.ru>
2018-09-27 12:07:18 +0300

    use recycler view

--- app/src/main/java/relsys/eu/coderswag/controller/MainActivity.kt
+++ app/src/main/java/relsys/eu/coderswag/controller/MainActivity.kt
@@ -2,20 +2,25 @@ package relsys.eu.coderswag.controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import relsys.eu.coderswag.R
-import relsys.eu.coderswag.adapter.CategoryAdapter
+import relsys.eu.coderswag.adapter.CategoryRecyclerAdapter
 import relsys.eu.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecyclerAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
