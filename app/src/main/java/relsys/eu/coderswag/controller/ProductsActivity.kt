1315ee8b43b449bf3ca2ce400e3c76f06fcb3e43
Kirill Mesiats <a55a4@yandex.ru>
2018-09-27 15:37:01 +0300

    create product adapter and finish the app

--- app/src/main/java/relsys/eu/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/relsys/eu/coderswag/controller/ProductsActivity.kt
@@ -1,11 +1,18 @@
 package relsys.eu.coderswag.controller
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_products.*
 import relsys.eu.coderswag.R
+import relsys.eu.coderswag.adapter.ProductsAdapter
+import relsys.eu.coderswag.services.DataService
 import relsys.eu.coderswag.utilities.EXTRA_CATEGORY
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var productAdapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +20,21 @@ class ProductsActivity : AppCompatActivity() {
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
 
+        productAdapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        val orientation = resources.configuration.orientation
+        var spanCount = 2
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productListView.layoutManager = layoutManager
+        productListView.adapter = productAdapter
     }
 }
